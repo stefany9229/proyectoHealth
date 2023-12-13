4ca10ee91809852a56d6d47564f9467dfc5c0c8e
StefanySB <stefany9209@gmail.com>
2023-12-13 18:46:43 -0500

    primer commit

---
+++ health/src/main/java/com/example/health/controller/health/HealthController.java
@@ -0,0 +1,12 @@
+package com.example.health.controller.health;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @RequestMapping("/health")
+    public String checkAPI(){
+        return "<h1>The API is working ok!</h1>"
+    }
+}
